4dfbd3e95a21e9dde39622390a779b895e41c68f
dushyantbhati2 <db90054@gmail.com>
2024-10-22 19:12:33 +0530

    Celery and redis dockerized

--- pyserver/command.md
+++ pyserver/command.md
@@ -1,3 +1,4 @@
 pip freeze > requirements.txt
 docker-compose up -d --build
 docker-compose down
+docker exec -it django /bin/sh
---
+++ pyserver/pyserver/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyserver.settings')
+
+app=Celery("pyserver")
+
+app.config_from_object("django.conf:settings",namespace="CELERY")
+
+@app.task
+def add_numbers():
+    return
+
+app.autodiscover_tasks()
